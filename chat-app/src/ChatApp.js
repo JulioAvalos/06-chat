d0e5916c4af29f8e7b5bf1b36b1740446ddb97f3
JulioAvalos <julioavalos2@msn.com>
2021-08-04 13:15:31 -0600

    adding pages and routes

--- chat-app/src/ChatApp.js
+++ chat-app/src/ChatApp.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import { AppRouter } from './router/AppRouter'
 
 export const ChatApp = () => {
     return (
         <div>
-            Chat app
+            <AppRouter />
         </div>
     )
 }
