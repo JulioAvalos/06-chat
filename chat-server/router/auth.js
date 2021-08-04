1c1ea95ff23facd40ac254a6134c55fc473ec6fe
JulioAvalos <julioavalos2@msn.com>
2021-08-03 22:36:59 -0600

    adding auth routes

---
+++ chat-server/router/auth.js
@@ -0,0 +1,38 @@
+/**
+ * path: ???
+ */
+const { Router } = require("express");
+
+const router = Router();
+
+// Crear nuevos usuarios
+router.post("/new", (req, res) => {
+
+  res.json({
+    ok: true,
+    msg: "new",
+  });
+
+});
+
+// Realizar login
+router.post("/", (req, res) => {
+
+  res.json({
+    ok: true,
+    msg: "login",
+  });
+
+});
+
+// Revalidar token
+router.get("/renew", (req, res) => {
+
+  res.json({
+    ok: true,
+    msg: "renew",
+  });
+  
+});
+
+module.exports = router;
