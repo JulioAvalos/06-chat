1ce2bc719ce9f8978deb293e74eb2c83258b7477
JulioAvalos <julioavalos2@msn.com>
2021-08-04 22:56:30 -0600

    adding logout logic

--- chat-app/src/auth/AuthContext.js
+++ chat-app/src/auth/AuthContext.js
@@ -101,7 +101,13 @@ export const AuthProvider = ({ children }) => {
 
   }, []);
 
-  const logout = () => {};
+  const logout = () => {
+    localStorage.removeItem('token');
+    setAuth({
+      checking: false,
+      logged: false
+    });
+  };
 
   return (
     <AuthContext.Provider
